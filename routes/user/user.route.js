37433bf4e776b31debd6b4971741134695692577
Christoffer Noerbjerg <chrisnorbjerg@gmail.com>
2021-02-19 11:59:30 +0100

    swagger hello world

--- routes/user/user.route.js
+++ routes/user/user.route.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('./user.controller')
 
-/*
- * @swagger
- * /user:
- *  get:
- *    description: asd
+/**
+ *  @swagger
+ *  /user:
+ *    get:
+ *      summary: Hello world endpoint
+ *      description: Temporary hello world endpoint for testing purposes
+ *      responses:
+ *          description: A string
+ *          content: application/json
  */
 router.get('', userController.getWorld)
 
