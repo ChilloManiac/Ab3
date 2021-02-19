2f94c30a19773c1bcfff036ad7772e05632fa946
Kristian Lau Jespersen <201807157@post.au.dk>
2021-02-19 11:11:50 +0100

    added rooms to hotels

--- models/hotel.js
+++ models/hotel.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
-    
+Room = require('./room');
+
 const hotelSchema = new mongoose.Schema({
     HotelName: { type: String, required: true },
     StreetName: { type: String, required: true },
     HouseNumber: { type: Number, required: true },
     Zip: { type: Number, required: true },
-    
+    Rooms: {type: [Room], required: true}
 });
 const Hotel = mongoose.model('Hotel', hotelSchema);
 Hotel.createIndexes();
