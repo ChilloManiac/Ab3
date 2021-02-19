9331f4d8de1b6fe04d26b21f9b20ae96ec2fad2b
bimbambendix <55709387+bimbambendix@users.noreply.github.com>
2021-02-19 11:32:38 +0100

    Swagger setup

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var hotelRouter = require('./routes/hotel/hotel');
+var roomRouter = require('./routes/room/room');
+var userRouter = require('./routes/user/user');
+
+// Swagger configuration
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API to manage Johns Hotel & Soen',
+      version: '1.0.0',
+      description: 'This is a REST API application made with Express for Johns Hotel & Soen.',
+      license: {
+        name: 'John has no license',
+        url: 'https://nolicence.co.uk.com.dk',
+      },
+      contact: {
+        name: 'John Johnson',
+        url: 'https://johnson.dk',
+        email: 'john@hotelogsoen.dk',
+      },
+      servers: [{
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      }, ],
+    },
+  };
+
+const options = {
+    swaggerDefinition,
+    apis: ['./routes/*.js']
+}
+
+const swaggerSpec = swaggerJSDoc(options);
 
 var app = express();
 
@@ -13,6 +50,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/hotel', hotelRouter);
+app.use('/room', roomRouter);
+app.use('/user', userRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+  
+// error handler
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json('Error: ' + (err.message || 'Internal server error'));
+});
 
 module.exports = app;
