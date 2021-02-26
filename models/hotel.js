dcba88ec54d02e58203781000de236c2462513e9
Kristian Lau Jespersen <201807157@post.au.dk>
2021-02-26 11:32:17 +0100

    moved roomSchema to hotel.js

--- models/hotel.js
+++ models/hotel.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
-Room = require('./room');
+
+const roomSchema = new mongoose.Schema({
+    roomNumber: { type: Number, required: true },
+    numberOfBeds: { type: Number, required: true },
+    isOccupied: { type: Boolean, required: true }
+});
 
 const hotelSchema = new mongoose.Schema({
     name: { type: String, unique: true, required: true },
     streetName: { type: String, required: true },
     houseNumber: { type: Number, required: true },
     zip: { type: Number, required: true },
-    rooms: {type: [Room], required: true}
+    rooms: {type: [roomSchema], required: true}
 });
 const Hotel = mongoose.model('Hotel', hotelSchema);
 Hotel.createIndexes();
--- routes/room/room.controller.js
+++ routes/room/room.controller.js
@@ -1,16 +1,43 @@
 const roomService = require('./room.service');
 const { HttpError } = require("../../middleware/errorHandler");
 
-function getWorld(req, res, next) {
-    roomService.HelloWorld().then((world) => res.json(world))
+function getRoomsByHotel(req, res, next) {
+    if (!req.body.hotel) {
+        throw new HttpError(400, "Hotel must be specified.");
+      }
+      roomService
+      .getRoomsByHotel(req.body.hotel)
+      .then((response) => res.status(200).send(response))
+      .catch((error) => next(error));
 }
 
 function getVacantRoomsByHotel(req, res, next) {
     if (!req.body.hotel) {
         throw new HttpError(400, "Hotel must be specified.");
       }
-    roomService.getVacantRoomsByHotel(req.body.hotel);
+      roomService
+      .getVacantRoomsByHotel(req.body.hotel)
+      .then((response) => res.status(200).send(response))
+      .catch((error) => next(error));
 }
+
+function getRoomByRoomNumber(req, res, next) {
+    if (!req.body.hotel || req.body.number) {
+        throw new HttpError(400, "Hotel and roomnumber must be specified.");
+      }
+      roomService
+      .getRoomByRoomNumber(req.body.hotel, number)
+      .then((response) => res.status(200).send(response))
+      .catch((error) => next(error));
+}
+
+router.post("/getRoomsByHotel", [verifyToken], roomController.getRoomsByHotel);
+router.post("/getVacantRoomsByHotel", [verifyToken], roomController.getVacantRoomsByHotel);
+router.post("/getRoomByRoomNumber", [verifyToken], roomController.getRoomByRoomNumber);
+router.post("/createRoom", [verifyToken], roomController.createRoom);
+router.post("/markRoomAsVacant", [verifyToken], roomController.markRoomAsVacant);
+router.post("/markRoomAsOccupied", [verifyToken], roomController.markRoomAsOccupied);
+router.post("/getHotelByName", [verifyToken], roomController.getHotelByName);
 
 
 module.exports = {
