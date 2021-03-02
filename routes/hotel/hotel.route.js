19d75cdfe31e17ded19c97bba86164c68ee3c323
Christoffer Noerbjerg <chrisnorbjerg@gmail.com>
2021-03-02 17:21:38 +0100

    swagger

--- routes/hotel/hotel.route.js
+++ routes/hotel/hotel.route.js
@@ -58,37 +58,86 @@ const hotelController = require('./hotel.controller')
  *    post:
  *      summary: Create a hotel
  *      description: Create a hotel if you are a manager
- *      parameters:
- *        - in: body
- *          name: body
- *          schema:
- *            type: object
- *            properties:
- *              hotelName:
- *                type: string
- *                required: true
- *                description: Name of the hotel
- *                example: Hotel Hallojsa
- *              streetName:
- *                type: string
- *                required: true
- *                description: Name of the street
- *                example: Hallojsa Vej
- *              houseNumber:
- *                type: number
- *                required: true
- *                example: 123
- *                description: Housenumber on the street
- *              zip:
- *                type: number
- *                required: true
- *                example: 8000
- *                description: Zip number for the adress
+ *      requestBody:
+ *        description: AAAAAAAAAAAAAAAAAAAAAA
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                hotelName:
+ *                  type: string
+ *                  required: true
+ *                  description: Name of the hotel
+ *                  example: Hotel Hallojsa
+ *                streetName:
+ *                  type: string
+ *                  required: true
+ *                  description: Name of the street
+ *                  example: Hallojsa Vej
+ *                houseNumber:
+ *                  type: number
+ *                  required: true
+ *                  example: 123
+ *                  description: Housenumber on the street
+ *                zip:
+ *                  type: number
+ *                  required: true
+ *                  example: 8000
+ *                  description: Zip number for the adress
  *      responses:
  *        201:
+ *          description: A newly created hotel
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  name:
+ *                    type: string
+ *                    description: Name of the hotel
+ *                    example: Hotel Hallojsa
+ *                  streetName:
+ *                    type: string
+ *                    description: Name of the street
+ *                    example: Hallojsa Vej
+ *                  houseNumber:
+ *                    type: number
+ *                    description: The streenumber
+ *                    example: 123
+ *                  zip:
+ *                    type: number
+ *                    description: Zip code of the hotels location
+ *                    example: 8000
+ *                  owner:
+ *                    type: string
+ *                    description: Username of the manager that created the hotel
+ *                    example: Don John Johnson
+ *                  rooms:
+ *                    type: array
+ *                    example: []
+ *                    items:
+ *                      type: object
+ *                      properties:
+ *                        roomNumber:
+ *                          type: number
+ *                          description: The number of the room
+ *                          example: 42
+ *                        numberOfBeds:
+ *                          type: number
+ *                          description: The amount of sleeping spaces in a room
+ *                          example: 4
+ *                        isOccupied:
+ *                          type: boolean
+ *                          description: True if the room is already occupied
+ *                          example: true
  *        400:
+ *          description: Bad request - Will be thrown if values are missing
  *        401:
+ *          description: Unauthorized - Will be thrown if the user is not logged in (Bearer token)
  *        403:
+ *           description: Forbidden - Will be thrown if the user is not in the 'manager' role
  *
  */
 
@@ -109,7 +158,7 @@ router.post('', hotelController.addHotel)
  *          description: Name of the hotel
  *      responses:
  *        200:
- *          description: A list of hotels
+ *          description: A hotel
  *          content:
  *            application/json:
  *              schema:
