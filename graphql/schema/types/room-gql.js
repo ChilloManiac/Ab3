35afa38cfa5300feb763ac5dc566ee6e935e7519
Kristian Lau Jespersen <201807157@post.au.dk>
2021-03-19 10:15:13 +0100

    removed resolvers

--- graphql/schema/types/room-gql.js
+++ graphql/schema/types/room-gql.js
@@ -9,18 +9,15 @@ const RoomGql = new GraphQLObjectType({
     name: 'Room',
     fields: {
         roomNumber: {
-            type: new GraphQLNonNull(GraphQLInt),
-            resolve: (source)=>{context.roomNumber}
+            type: new GraphQLNonNull(GraphQLInt)
 
         },
         numberOfBeds: {
-            type: new GraphQLNonNull(GraphQLInt),
-            resolve: (context)=>{context.roomNumber}
+            type: new GraphQLNonNull(GraphQLInt)
 
         },
         isOccupied: {
-            type: new GraphQLNonNull(GraphQLBoolean),
-            resolve: (context)=>{context.isOccupied}
+            type: new GraphQLNonNull(GraphQLBoolean)
 
         }
     }
